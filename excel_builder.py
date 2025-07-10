9465deb4d4a04569b9a1c778d3cf7687e48575d3
mamutpeter <nikitamailbox2018@gmail.com>
2025-07-10 18:24:20 +0300

    Update excel_builder.py

--- excel_builder.py
+++ excel_builder.py
@@ -1,9 +1,6 @@
-from openpyxl import Workbook
+import pandas as pd
 
-def save_table_to_excel(table: list[list[str]], path: str) -> str:
-    wb = Workbook()
-    ws = wb.active
-    for row in table:
-        ws.append(row)
-    wb.save(path)
-    return path
+def save_table_to_excel(table_data: list, file_path: str) -> str:
+    df = pd.DataFrame(table_data)
+    df.to_excel(file_path, index=False)
+    return file_path
