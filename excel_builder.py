80551bb64d0eff49edab58ff25b27cd8d18f54c3
mamutpeter <nikitamailbox2018@gmail.com>
2025-07-10 18:47:02 +0300

    Update excel_builder.py

--- excel_builder.py
+++ excel_builder.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def save_table_to_excel(table_data: list, file_path: str) -> str:
+def save_table_to_excel(table_data, file_path: str) -> str:
     df = pd.DataFrame(table_data)
     df.to_excel(file_path, index=False)
     return file_path
